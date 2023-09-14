a288c4689013c4d5dda9941be1286241bbc439b6
Rarii Mathis <juniormathis82@gmail.com>
2023-09-14 16:28:30 -0400

    form

--- js/challenge.js
+++ js/challenge.js
@@ -3,26 +3,45 @@
 //     const counter= document.getElementById('counter')
 //     console.log(counter)
 //     counter.textContent=""
+
+const { applyDocumentFeatures } = require("jsdom/lib/jsdom/browser/documentfeatures")
+
 // }
-const counter= document.getElementById('counter')
+const counter = document.getElementById('counter')
 // click listener on buttons
 //minus button decrement
-const minusButton= document.getElementById('minus')
+const minusButton = document.getElementById('minus').addEventListener("click",updateCounterMinus)
+const addButton = document.getElementById('plus').addEventListener("click",updateCounterPlus)
 console.log(minusButton)
 let count=0
-function updateCounter(){
+function updateCounterMinus(){
     counter.textContent=count
     count--
-    console.log('I am pressed')
+    // console.log('I am pressed')
 }
-function minusButtonClicked(){
-    addEventListener('click',updateCounter)
-    console.log('pressed')
+function updateCounterPlus(){
+    counter.textContent=count
+    count++
+    // console.log('I am pressed')
 }
-minusButtonClicked()
-function forListnerMinus(){
-    console.log('I was pressed')
 
-}
 
 // form
+
+const form = document.getElementById('comment-form')
+
+form.addEventListener("submit", (e) => addComment(e))
+
+function addComment(e) {
+    e.preventDefault()
+    const commentInput = document.getElementById('comment-input')
+   commentInput.textContent.e.target.comment.value
+   console.log()
+
+
+}
+
+// const newDiv = document.createElement('div')
+// newDiv.textContent.e.target.comment.vaule
+// //    comment-Input.append('newDiv')
+ console.log(e.target.comment.value)
