7599c59247f3c3dbdd6e3b9534be23b3b3d5b4c7
Rarii Mathis <juniormathis82@gmail.com>
2023-09-14 18:59:41 -0400

    form fix

--- js/challenge.js
+++ js/challenge.js
@@ -4,15 +4,15 @@
 //     console.log(counter)
 //     counter.textContent=""
 
-const { applyDocumentFeatures } = require("jsdom/lib/jsdom/browser/documentfeatures")
+// const { applyDocumentFeatures } = require("jsdom/lib/jsdom/browser/documentfeatures")
 
 // }
-const counter = document.getElementById('counter')
+const counter = document.getElementById('counter');
 // click listener on buttons
 //minus button decrement
-const minusButton = document.getElementById('minus').addEventListener("click",updateCounterMinus)
-const addButton = document.getElementById('plus').addEventListener("click",updateCounterPlus)
-console.log(minusButton)
+const minusButton = document.getElementById('minus').addEventListener("click",updateCounterMinus);
+const addButton = document.getElementById('plus').addEventListener("click",updateCounterPlus);
+// console.log(minusButton)
 let count=0
 function updateCounterMinus(){
     counter.textContent=count
@@ -20,28 +20,27 @@ function updateCounterMinus(){
     // console.log('I am pressed')
 }
 function updateCounterPlus(){
-    counter.textContent=count
-    count++
+    counter.textContent = count
+    count ++;
     // console.log('I am pressed')
 }
 
 
 // form
 
-const form = document.getElementById('comment-form')
+const form = document.getElementById('comment-form');
 
-form.addEventListener("submit", (e) => addComment(e))
+form.addEventListener("submit", (e) => addComment(e));
 
 function addComment(e) {
     e.preventDefault()
-    const commentInput = document.getElementById('comment-input')
-   commentInput.textContent.e.target.comment.value
-   console.log()
-
-
+    const commentInput = document.getElementById('comment-input');
+    commentInput.value;
+    console.log(commentInput.value);
+    
+    const newDiv = document.createElement('div')
+    newDiv.textContent = commentInput.value
+    const newComment = document.getElementById('list')
+    newComment.append(newDiv)
 }
 
-// const newDiv = document.createElement('div')
-// newDiv.textContent.e.target.comment.vaule
-// //    comment-Input.append('newDiv')
- console.log(e.target.comment.value)
